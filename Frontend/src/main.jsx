2b47ef16538ae4c9b7a80c4abf11d5570e768457
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-06-04 20:51:17 +0530

    added react router config and setup router

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import Register from "./components/main_Components/Register.jsx";
+import Login from "./components/main_Components/Login.jsx";
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>Hello World!</h1>
+    <RouterProvider router={route} />
   </StrictMode>,
 );
